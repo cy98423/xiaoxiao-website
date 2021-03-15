ab5a36d1b7d5f18f384c38d3c78c66281e195e78
Cy-Qy <308103222@qq.com>
2021-03-15 21:39:07 +0800

    deploy

---
+++ precache-manifest.76fa930c1fb002e55a7fbfae1107ac03.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a795e46209dd2749e35c",
+    "url": "/xiaoxiao-website/css/app.5d16b96e.css"
+  },
+  {
+    "revision": "8b9aba02671b9a72ce48",
+    "url": "/xiaoxiao-website/css/chunk-vendors.6afb7b53.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/xiaoxiao-website/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/xiaoxiao-website/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "2adbdbd64b9fe15751021c3cbc522bb9",
+    "url": "/xiaoxiao-website/index.html"
+  },
+  {
+    "revision": "a795e46209dd2749e35c",
+    "url": "/xiaoxiao-website/js/app.b0c831f1.js"
+  },
+  {
+    "revision": "8b9aba02671b9a72ce48",
+    "url": "/xiaoxiao-website/js/chunk-vendors.46361bf7.js"
+  },
+  {
+    "revision": "acb3d25d13902ce64ec7e7d63b7d4df1",
+    "url": "/xiaoxiao-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/xiaoxiao-website/robots.txt"
+  }
+]);
